079e65c904765f25fc8c8cbdee32f935ad744846
maopingxiong <58541195+maopingxiong@users.noreply.github.com>
2022-07-08 10:46:47 +0800

    Create user.java

---
+++ user.java
@@ -0,0 +1,26 @@
+class UserSolution {
+
+	void init(int N, int px[], int py[]) {
+
+	}
+
+	void addRestaurant(int pID, int x, int y) {
+
+	}
+
+	void removeRestaurant(int pID) {
+
+	}
+
+	void order(int uID, int pID) {
+
+	}
+
+	void beFriend(int uID1, int uID2) {
+
+	}
+
+	int recommend(int uID) {
+		return 0;
+	}
+}
