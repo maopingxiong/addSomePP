d58650a728c8f42ba65e940dc8cc4fd4e0c696d9
maopingxiong <58541195+maopingxiong@users.noreply.github.com>
2022-07-29 18:11:17 +0800

    Create User.cpp

---
+++ Parking Lot/User.cpp
@@ -0,0 +1,11 @@
+void init(int mBaseTime, int mBaseFee, int mUnitTime, int mUnitFee, int mCapacity) {
+	return;
+}
+
+int arrive(int mTime, int mCar) {
+	return 0;
+}
+
+int leave(int mTime, int mCar) {
+	return 0;
+}
