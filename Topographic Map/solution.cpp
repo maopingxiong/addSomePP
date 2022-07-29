869564f62f7ca243b91146920828cdbdbd43d540
maopingxiong <58541195+maopingxiong@users.noreply.github.com>
2022-07-29 18:22:44 +0800

    Create solution.cpp

---
+++ Topographic Map/solution.cpp
@@ -0,0 +1,22 @@
+#define MAX_MAP_SIZE 2000
+
+void init(int N, int K, int mHeight[][MAX_MAP_SIZE])
+{
+}
+
+void query(Point mA, Point mB, int mCount, Point mTop[]) 
+{
+}
+
+int getHeight(Point mP)
+{
+	return 0;
+}
+
+void work(Point mA, Point mB, int mH)
+{
+}
+
+void destroy() 
+{
+}
